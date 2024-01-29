41f0e93426507c2c9f60d33ce3a73bffcf405755
mk3l <maikel.angueira@generalsoftwareinc.com>
2024-01-29 10:40:42 -0500

    Inject IConfiguration to Connection factory to create new DbContext

--- src/Infrastructure/Data/Configurations/AppDbConnectionFactory.cs
+++ src/Infrastructure/Data/Configurations/AppDbConnectionFactory.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using StoreOnline.Application.Batch.Commands;
 using StoreOnline.Application.Common.Interfaces;
 
 namespace StoreOnline.Infrastructure.Data.Configurations;
 
-public class AppDbConnectionFactory : IDbContextFactory
+public class AppDbConnectionFactory(IConfiguration configuration) : IDbContextFactory
 {
     public IApplicationDbContext NewDbContext()
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
         DbContextOptionsBuilder<ApplicationDbContext> builder = new();
-        builder.UseSqlite("DataSource=app.db;Cache=Shared");
+        builder.UseSqlite(connectionString);
         return new ApplicationDbContext(builder.Options);
     }
 }
