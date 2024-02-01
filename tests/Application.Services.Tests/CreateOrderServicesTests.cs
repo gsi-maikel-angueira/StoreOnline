fb85ae7b2b71d683168c21d7b53fe6b4231f324e
mk3l <maikel.angueira@generalsoftwareinc.com>
2024-02-01 14:27:33 -0500

    Create new test to check product stock decrement

--- tests/Application.Services.Tests/CreateOrderServicesTests.cs
+++ tests/Application.Services.Tests/CreateOrderServicesTests.cs
@@ -54,4 +54,34 @@ public class CreateOrderServicesTests
         newOrder.OrderDetails.Should().NotBeEmpty();
         newOrder.OrderDetails.Count.Should().Be(2);
     }
+    
+    [Test]
+    public async Task ShouldDecrementProductOnStock()
+    {
+        _mockOrderRepository.Setup(m => m.AddAsync(It.IsAny<Order>()))
+            .ReturnsAsync(CreatedOrder);
+        _mockProductReadRepository.Setup(m => m.FindByIdAsync(ProductA.Id)).ReturnsAsync(() =>ProductA);
+        _mockProductReadRepository.Setup(m => m.FindByIdAsync(ProductB.Id)).ReturnsAsync(() =>ProductB);
+        _mockOrderDetailRepository.Setup(m => m.AddAsync(It.Is<OrderDetail>(o => o.Product!.Id == ProductA.Id)))
+            .ReturnsAsync(() => OrderDetailA);
+        _mockOrderDetailRepository.Setup(m => m.AddAsync(It.Is<OrderDetail>(o => o.Product!.Id == ProductB.Id)))
+            .ReturnsAsync(() => OrderDetailB);
+        CreateOrderServices createOrderServices =
+            new(
+                _mockOrderRepository.Object,
+                _mockProductReadRepository.Object,
+                _mockOrderDetailRepository.Object);
+
+        CreateOrderCommand createCommandRequest = new()
+        {
+            CustomerId = 1, Products = new List<ProductDto> { ProductDtoA, ProductDtoB }
+        };
+
+        var newOrder = await createOrderServices.CreateOrUpdateAsync(createCommandRequest);
+        _mockProductReadRepository.Verify(repository => repository.FindByIdAsync(It.IsAny<int>()), Times.Exactly(2));
+        newOrder.Should().NotBeNull();
+        newOrder.OrderDetails.Should().NotBeEmpty();
+        ProductA.Stock.Should().Be(45);
+        ProductB.Stock.Should().Be(10);
+    }
 }
